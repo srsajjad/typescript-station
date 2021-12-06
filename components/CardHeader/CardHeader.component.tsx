fc68dc4d621e2c9e73d6447195aa9b8a0c9d4849
SR Sajjad <sajjadsr001@gmail.com>
2021-12-06 14:29:37 +0600

    add header design

--- components/CardHeader/CardHeader.component.tsx
+++ components/CardHeader/CardHeader.component.tsx
@@ -1,14 +1,19 @@
+import { IconPower, IconChevronLeft } from "@tabler/icons";
 import React from "react";
 import styles from "./CardHeader.module.css";
 
 export const CardHeader = () => {
-  const { card_header } = styles;
+  const { card_header, header_title, icon_left, icon_right } = styles;
 
   return (
     <div className={card_header}>
-      <div>back</div>
-      <div>STATIONS</div>
-      <div>links</div>
+      <button className={icon_left}>
+        <IconChevronLeft size={35} color="white" />
+      </button>
+      <div className={header_title}>STATIONS</div>
+      <button className={icon_right}>
+        <IconPower size={35} color="white" />
+      </button>
     </div>
   );
 };
